d14b87fe40895814ce86b8e93fb610e092e3d8b4
Robert Spinean <robispinean@yahoo.com>
2021-11-01 19:42:46 +0200

    Init servertest

--- src/test/java/WebServerTests.java
+++ src/test/java/WebServerTests.java
@@ -1,3 +1,18 @@
+import org.junit.After;
+import org.junit.Before;
+
+import static org.mockito.Mockito.*;
+
 public class WebServerTests {
+    WebServer server;
+    WebServer webServerMock = mock(WebServer.class);
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
 
 }
