9a245f339f3b92b00dc3aa43522048575e9d0cee
Robert Spinean <robispinean@yahoo.com>
2021-11-01 20:11:26 +0200

    Add run test

--- src/test/java/WebServerTests.java
+++ src/test/java/WebServerTests.java
@@ -1,6 +1,11 @@
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class WebServerTests {
@@ -15,4 +20,15 @@ public class WebServerTests {
     public void tearDown() throws Exception {
     }
 
+    @Test
+    public void testRun() throws IOException {
+        String[] args = null;
+        server.main(args);
+        try {
+            ServerSocket serverSocket = new ServerSocket(8080);
+            server.main(args);
+        } catch (Exception e) {
+            System.out.println(e.getStackTrace());
+        }
+    }
 }
